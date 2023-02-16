527f856febe0c03744cfce37aa65935b45919d33
xychenxy <mavischen916@gmail.com>
2023-02-16 14:04:08 +1100

    use nginx to proxy client and server

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+
+const api = require("./routes/api");
+const Config = require("./config");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+// app.use(express.static(path.join(__dirname, "..", "..", "react-app", "dist")));
+
+app.get("/", (req, res) => {
+	res.send("Hi");
+});
+
+app.use("/v1", api);
+
+// if (["prod"].includes(Config.envName)) {
+// 	//Set static folder
+// 	app.use(express.static(path.join(__dirname, "..", "dist")));
+// 	app.get("/*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
+// 	});
+// }
+
+module.exports = app;
